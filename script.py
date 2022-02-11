b3d75d36ce11146fcc35f2fe2be2197c3d96195c
Davi de Paula Oliveira <davi.miau@gmail.com>
2022-02-11 14:45:20 -0300

    Commit

--- script.py
+++ script.py
@@ -6,34 +6,41 @@ path = 'instances/1'
 
 files = [f for f in listdir(path) if isfile(join(path, f))]
 
+i = 1
+total = len(files)
+
 for file in files:
     # os.system("python archetti_ext-heur.py instances/1/" + file + " > instances/1-archetti-tempo/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/1000/1.1/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/1000/1.2/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/1000/1.3/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/1000/1.4/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/1000/1.5/" + file)
+    print(f"Estou no arquivo {i} de {total}...")
 
-    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/2500/1.1/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/2500/1.2/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/2500/1.3/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/2500/1.4/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/2500/1.5/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/novo/1000/1.1/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/novo/1000/1.2/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/novo/1000/1.3/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/novo/1000/1.4/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 1000 > instances/Gdowska-otimizado/novo/1000/1.5/" + file)
 
-    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/5000/1.1/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/5000/1.2/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/5000/1.3/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/5000/1.4/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/5000/1.5/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/novo/2500/1.1/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/novo/2500/1.2/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/novo/2500/1.3/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/novo/2500/1.4/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 2500 > instances/Gdowska-otimizado/novo/2500/1.5/" + file)
 
-    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/7500/1.1/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/7500/1.2/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/7500/1.3/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/7500/1.4/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/7500/1.5/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/novo/5000/1.1/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/novo/5000/1.2/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/novo/5000/1.3/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/novo/5000/1.4/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 5000 > instances/Gdowska-otimizado/novo/5000/1.5/" + file)
 
-    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/10000/1.1/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/10000/1.2/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/10000/1.3/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/10000/1.4/" + file)
-    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/10000/1.5/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/novo/7500/1.1/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/novo/7500/1.2/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/novo/7500/1.3/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/novo/7500/1.4/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 7500 > instances/Gdowska-otimizado/novo/7500/1.5/" + file)
+
+    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/novo/10000/1.1/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/novo/10000/1.2/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/novo/10000/1.3/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/novo/10000/1.4/" + file)
+    os.system("python gdowska_exact.py instances/1/" + file + " 10000 > instances/Gdowska-otimizado/novo/10000/1.5/" + file)
+
+    i = i + 1
