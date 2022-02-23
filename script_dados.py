e507e31aabe92f5261a85349769fbaac0096c45c
Davi de Paula Oliveira <davi.miau@gmail.com>
2022-02-23 19:45:36 -0300

    Create script_dados.py

---
+++ script_dados.py
@@ -0,0 +1,62 @@
+from os import listdir
+from os.path import isfile, join
+
+path = 'instances/Gdowska/novo/1.1'
+
+files = [f for f in listdir(path) if isfile(join(path, f))]
+
+for file in files:
+    arquivo1 = open('instances/Gdowska/novo/1.1/' + file, 'r')
+    conteudo_arquivo1 = []
+
+    arquivo2 = open('instances/Gdowska/novo/1.2/' + file, 'r')
+    conteudo_arquivo2 = []
+
+    arquivo3 = open('instances/Gdowska/novo/1.3/' + file, 'r')
+    conteudo_arquivo3 = []
+
+    arquivo4 = open('instances/Gdowska/novo/1.4/' + file, 'r')
+    conteudo_arquivo4 = []
+
+    arquivo5 = open('instances/Gdowska/novo/1.5/' + file, 'r')
+    conteudo_arquivo5 = []
+
+    for linha in arquivo1:
+        linha = linha.strip()
+        conteudo_arquivo1.append(linha)
+
+    for linha in arquivo2:
+        linha = linha.strip()
+        conteudo_arquivo2.append(linha)
+
+    for linha in arquivo3:
+        linha = linha.strip()
+        conteudo_arquivo3.append(linha)
+
+    for linha in arquivo4:
+        linha = linha.strip()
+        conteudo_arquivo4.append(linha)
+
+    for linha in arquivo5:
+        linha = linha.strip()
+        conteudo_arquivo5.append(linha)
+
+    linha1 = conteudo_arquivo1[7].split() # mudar
+    linha2 = conteudo_arquivo2[7].split() # mudar
+    linha3 = conteudo_arquivo3[7].split() # mudar
+    linha4 = conteudo_arquivo4[7].split() # mudar
+    linha5 = conteudo_arquivo5[7].split() # mudar
+
+    numero1 = float(linha1[0])
+    numero2 = float(linha2[0])
+    numero3 = float(linha3[0])
+    numero4 = float(linha4[0])
+    numero5 = float(linha5[0])
+
+    print(f"{round((numero1 + numero2 + numero3 + numero4 + numero5) / 5, 2)}")
+
+    arquivo1.close()
+    arquivo2.close()
+    arquivo3.close()
+    arquivo4.close()
+    arquivo5.close()
